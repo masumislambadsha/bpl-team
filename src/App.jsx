8c00145be71f27603326d363a8a3554d7433d571
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-22 23:44:55 +0600

    update change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/Hero-Section/Hero-Section";
 import AvailablePLayers from "./components/AvailablePlayers/AvailablePLayers";
 import SelectedPlayers from "./components/selectedPlayers/SelectedPlayers";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Subscribe from "./components/Subscribe/Subscribe";
 
@@ -13,21 +13,37 @@ const fetchPlayers = async () => {
 };
 function App() {
   const playersPromise = fetchPlayers();
+
+  const [toggle, setToggle] = useState(true)
+
   return (
     <>
       <div className="max-w-[1500px] mx-auto">
-         <Navbar></Navbar>
+        <Navbar></Navbar>
         <HeroSection></HeroSection>
-        <Suspense
+        <div className="flex justify-between px-[30px] py-[15px]">
+          <h1 className="font-bold text-2xl">Available Players</h1>
+          <div className="">
+            <button onClick={()=>setToggle(true)} className={`btn ${toggle===true?"bg-[#E7FE29]":''} px-[30px] py-[20px] rounded-l-2xl border-1 border-gray-200 border-r-0`}>Available</button>
+            <button onClick={()=>setToggle(false)} className={`btn ${toggle===false?"bg-[#E7FE29]":''} px-[30px] py-[20px] rounded-r-2xl border-1 border-gray-200 border-r-0`}>
+              Selected <span>(0)</span>
+            </button>
+          </div>
+        </div>
+
+        {
+          toggle=== true? <Suspense
           fallback={
             <span className="loading loading-spinner loading-xl mx-auto ml-[650px]"></span>
           }
         >
           <AvailablePLayers playersPromise={playersPromise}></AvailablePLayers>
-        </Suspense>
+        </Suspense>:<Suspense fallback={<span className="loading loading-spinner loading-xl mx-auto ml-[650px]"></span>}><SelectedPlayers></SelectedPlayers></Suspense>
+        }
 
-         <SelectedPlayers></SelectedPlayers>
-          <Subscribe></Subscribe>
+
+
+        <Subscribe></Subscribe>
         <Footer></Footer>
       </div>
     </>
--- src/components/PlayerCard/PlayerCart.jsx
+++ src/components/PlayerCard/PlayerCart.jsx
@@ -1,62 +1,53 @@
-import React, { use } from "react";
+import React from 'react';
 import profileIcon from "../../assets/Group.png";
 import flagIcon from "../../assets/flag.png";
-import PlayerCart from "../PlayerCard/PlayerCart";
 
-const AvailablePLayers = ({ playersPromise }) => {
-  const playersData = use(playersPromise);
-  console.log(playersData);
-
+const PlayerCart = ({player}) => {
   return (
-    <div className="mt-[100px] flex flex-wrap md:gap-24 space-y-5 justify-center items-center">
-
-    {
-      playersData.map(player =><PlayerCart player={player}></PlayerCart> )
-    }
-
-      {/* <div className="card bg-base-100 w-96 border-1 p-4 border-gray-200">
+    <div>
+      <div className="cursor-pointer card bg-base-100 w-96 border-1 p-4 border-gray-200">
         <figure>
           <img
-            className="rounded-2xl"
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            className="rounded-2xl w-[350px] h-[350px] object-cover"
+            src={player.player_image}
             alt="Shoes"
           />
         </figure>
         <div className="card-body px-0">
           <h2 className="card-title mb-5">
-            <img src={profileIcon} /> Card Title{" "}
+            <img src={profileIcon} /> {player.player_name}
           </h2>
           <div className="flex justify-between items-center border-b-1 border-gray-200">
             <div className="opacity-70 flex gap-3 items-center justify-center pl-4">
               <img src={flagIcon} alt="" />
-              <p>India</p>
+              <p>{player.player_name}</p>
             </div>
             <h4 className="font-regular text-[14px] btn btn-ghost text-[#131313]">
-              All-Rounder
+              {player.playing_role}
             </h4>
           </div>
           <div className="flex items-center justify-between">
             <h4 className="font-regular text-[14px] btn btn-ghost text-[#131313] ">
-              Left-Hand-Bat
+              {player.batting_style}
             </h4>
-            <p className="text-end pr-4">Left-Hand-Bat</p>
+            <p className="text-end pr-4">{player.bowling_style}</p>
           </div>
           <div className="flex justify-between">
             <p className="font-bold text-[16px] pl-4">Rating</p>
-            <p className="font-bold text-[16px] text-end pr-4">5</p>
+            <p className="font-bold text-[16px] text-end pr-4">{player.rating}</p>
           </div>
           <div className="flex justify-between items-center">
             <h4 className="font-semibold text-[14px] pl-4 text-[#131313] ">
-              Price: $1500000
+              Price: ${player.price_usd}
             </h4>
             <h4 className="font-regular text-[14px] btn btn-ghost text-[#131313]">
               Choose Player
             </h4>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
 
-export default AvailablePLayers;
+export default PlayerCart;
